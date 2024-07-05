7b3da1cb79cc8d148a9bbc0a8e1a69f0ed81513c
merve <merveeksii61@gmail.com>
2024-07-05 13:00:58 +0300

    mrv

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+var builder = WebApplication.CreateBuilder(args);
+
+//add services to the container
+builder.Services.AddControllersWithViews();
+
+var app:WebApplication = builder.Build();
+
+//Confige the HTTP request pipeline
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("Home/Error");
+    //The defaul HSTS valoue is 30 days. o may want to chage this for production scenarius, see https://aka.ms/aspnecore-hsts
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    nameof: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
