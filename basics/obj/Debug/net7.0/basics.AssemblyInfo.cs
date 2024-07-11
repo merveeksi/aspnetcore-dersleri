fa0fc90931554c786d6ef5912f157c48f7d049ea
MERVE EKŞİ <merveeksii61@gmail.com>
2024-07-11 16:56:30 +0300

    ma

--- basics/obj/Debug/net7.0/basics.AssemblyInfo.cs
+++ basics/obj/Debug/net7.0/basics.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("basics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a80cee75c30919543a7e66d484dd5d15abed37b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("basics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("basics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
